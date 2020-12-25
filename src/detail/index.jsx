8d6a269eaa07e2c6663fd99ef4d294f28cf771fc
Pyae Sone <mgmgpyaesonewin@gmail.com>
2020-12-25 21:04:00 +0630

    Validation handled and Loading state added

--- src/detail/index.jsx
+++ src/detail/index.jsx
@@ -6,6 +6,8 @@ import MiniCard from "../components/minicard";
 import Status from "../components/status";
 
 import "./index.css";
+import Loading from "../components/loading";
+import React from "react";
 
 const Detail =function(){
     let { lat,lng } = useParams();
@@ -14,27 +16,38 @@ const Detail =function(){
         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=d79e473c27e1ce85a145c3a51e23f2c5`
     );
 
-    return(
-        <div className="container">
-            <img
-                src={`http://openweathermap.org/img/wn/${currentWeatherState.data && currentWeatherState.data.current.weather[0].icon}@4x.png`}
-                alt="status" />
-            <p className="capitalize font-medium tracking-wider">{currentWeatherState.data && currentWeatherState.data.current.weather[0].description}</p>
-            <p className="text-7xl my-8">{currentWeatherState.data && currentWeatherState.data.current.temp}&#176;</p>
-            <div className="flex justify-center">
-                <Status icon={faWind} data={currentWeatherState.data && currentWeatherState.data.current.wind_speed} unit="km/h"/>
-                <Status icon={faTint} data={currentWeatherState.data && currentWeatherState.data.current.humidity} unit="%"/>
-            </div>
-            <div className="flex mt-8 overflow-y-scroll">
-                {
-                    currentWeatherState.data && currentWeatherState.data.daily.map((day) => (
-                        <MiniCard datetime={day.dt} iconcode={day.weather[0].icon} temperature={day.temp.max}/>
-                    ))
-                }
-            </div>
-        </div>
-    )
+    if(currentWeatherState && currentWeatherState.data && currentWeatherState.data.current) {
+        const { daily } = currentWeatherState.data;
+        const { temp, wind_speed, humidity } = currentWeatherState.data.current;
+        const { icon, description } = currentWeatherState.data.current.weather[0];
 
+        return(
+            <div className="container">
+                <img
+                    src={`http://openweathermap.org/img/wn/${icon}@4x.png`}
+                    alt="status" />
+                <p className="capitalize font-medium tracking-wider">{description}</p>
+                <p className="text-7xl my-8">{temp}&#176;</p>
+                <div className="flex justify-center">
+                    <Status icon={faWind} data={wind_speed} unit="km/h"/>
+                    <Status icon={faTint} data={humidity} unit="%"/>
+                </div>
+                <div className="flex mt-8 overflow-y-scroll">
+                    {
+                        daily.map(({ dt, weather, temp }, index) => (
+                            <MiniCard datetime={dt} iconcode={weather[0].icon} temperature={temp.max} key={index}/>
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <>
+                { currentWeatherState.loading && <Loading /> }
+            </>
+        )
+    }
 }
 
-export default Detail
+export default Detail;
