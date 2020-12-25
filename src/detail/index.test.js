aa896c387a44813473753342b10aa14a9baeabb5
win <winhtaikaung28@gmail.com>
2020-12-25 14:32:27 +0630

    fix Detail testcase

--- src/detail/index.test.js
+++ src/detail/index.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import Detail from '.';
+import {Route} from 'react-router'
 
 test('renders Detail component', () => {
-  render(<Detail />);
+  render( <Route path="/detail/:lat/:lng">
+  <Detail />
+</Route>);
   const linkElement = screen.getByText(/Detail/i);
   expect(linkElement).toBeInTheDocument();
 });
