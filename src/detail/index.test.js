a143cd0e30f0a6f3592deb70a3d4d5b2868e4e6a
win <winhtaikaung28@gmail.com>
2020-12-25 14:57:55 +0630

    fix test cases

--- src/detail/index.test.js
+++ src/detail/index.test.js
@@ -1,11 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import Detail from '.';
-import {Route} from 'react-router'
+import { render, screen } from "@testing-library/react";
+import Detail from ".";
+import { Router, Route } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
-test('renders Detail component', () => {
-  render( <Route path="/detail/:lat/:lng">
-  <Detail />
-</Route>);
+test("renders Detail component", () => {
+  const history = createMemoryHistory();
+  history.push("/detail/1.11/2.11");
+  render(
+    <Router history={history}>
+      <Route path="/detail/:lat/:lng">
+        <Detail />
+      </Route>
+    </Router>
+  );
   const linkElement = screen.getByText(/Detail/i);
   expect(linkElement).toBeInTheDocument();
 });
