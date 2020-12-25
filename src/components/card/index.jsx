944305d2fcaed197f509e0bfb5a9afa97aa2a777
win <winhtaikaung28@gmail.com>
2020-12-25 18:30:49 +0630

    map marker added

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,14 +1,22 @@
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+
 import "./index.css";
 const Card = ({ name, population }) => (
   <>
     <div data-testid="card" className="card">
-      <p data-testid="cityname" className="city">
-        {name}
-      </p>
-      <p data-testid="population" className="population">
-        {population}
-      </p>
+      <div className="card-row">
+        <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" />
+        <div className="card-col">
+          <p data-testid="cityname" className="city">
+            {name} Weather
+          </p>
+          <p data-testid="population" className="population">
+            {population} people
+          </p>
+        </div>
+      </div>
     </div>
   </>
 );
